02e7623c6d5cb245d2720b7caa25dcede48c99ac
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-11-27 21:25:46 +0000

    working location choice

--- src/interface.js
+++ src/interface.js
@@ -3,6 +3,15 @@ $(document).ready(function() {
   updateTemperature();
   updateSaverState();
 
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+    $('#city').text(city);
+  })
+
   $('#up').click(function() {
     thermostat.up();
     updateTemperature();
@@ -31,5 +40,14 @@ $(document).ready(function() {
   function updateTemperature() {
     $('#temperature').text(thermostat.getCurrentTemperature());
     $('#temperature').attr('class', thermostat.energy());
+  }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+     $('#current-temperature').text(data.main.temp);
+    })
   }
 });
